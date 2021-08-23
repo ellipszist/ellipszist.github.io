b43de5a6423033c9bb35bdc0104b58826565d26e
ELL <ellipszist@gmail.com>
2021-08-23 10:11:22 +0700

    Test: Lazy-Load images with ScrollOut

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,6 @@ function stickyElement(a){a=document.querySelector(".header");a=getComputedStyle
 
 /* Autoplay audio with mute and play */
 var pause=document.querySelector(".pause"),audio=document.querySelector(".audio");function togglePlay(){audio.paused?(audio.play(),pause.innerHTML="🔇"):(audio.pause(),pause.innerHTML="🏝️",pause.style.color=" #848484")};
+
+/* Lazy-Load images with ScrollOut */
+ScrollOut({targets:".photo",onShown:function(a){a.src||(a.src=a.dataset.src)}});
