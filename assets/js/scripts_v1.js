eb785c686e19db54cb8b5fc566ca812a57e55ba1
ELL <ellipszist@gmail.com>
2021-08-29 01:10:53 +0700

    Redesign

--- assets/js/scripts_v1.js
+++ assets/js/scripts_v1.js
@@ -2,4 +2,7 @@
 function stickyElement(a){a=document.querySelector(".header");a=getComputedStyle(a).height.split("px")[0];var b=document.querySelector(".navigation"),c=window.scrollY;c>a?b.classList.add("is-fixed"):c<a&&b.classList.remove("is-fixed")}window.addEventListener("scroll",stickyElement);
 
 /* Autoplay audio with mute and play */
-var pause=document.querySelector(".pause"),audio=document.querySelector(".audio");function togglePlay(){audio.paused?(audio.play(),pause.innerHTML="🔇"):(audio.pause(),pause.innerHTML="🏝️",pause.style.color="#848484")};
+var pause=document.querySelector(".pause"),audio=document.querySelector(".audio");function togglePlay(){audio.paused?(audio.play(),pause.innerHTML="🔇"):(audio.pause(),pause.innerHTML="🏝️",pause.style.color=" #848484")};
+
+/* Lazy-Load images with ScrollOut */
+ScrollOut({targets:".photo",onShown:function(a){a.src||(a.src=a.dataset.src)}});
