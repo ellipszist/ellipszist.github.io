3c9c902fd736652a21a64f04a0716b4360255208
ELL <ellipszist@gmail.com>
2023-02-06 23:17:32 +0700

    Update script.js

--- x/script.js
+++ x/script.js
@@ -5,12 +5,11 @@ document.getElementById("query").addEventListener("input", function() {
   const query = this.value;
   
   // fetch the game data
-  fetch("gameData.json")
+  fetch('gameData.json')
     .then(response => response.json())
     .then(data => {
+      const filteredData = data.filter(game => game.name.toLowerCase().includes(query.toLowerCase()));
       gamesContainer.innerHTML = "";
-
-      let filteredData = data.filter(game => game.name.toLowerCase().includes(query.toLowerCase()));
 
       if (filteredData.length === 0) {
         noResults.style.display = "block";
