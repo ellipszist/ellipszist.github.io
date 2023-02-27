a387cc21f09daa99f9bc013b196ae3aaabbdc4a2
ELL <ellipszist@gmail.com>
2023-02-27 16:56:48 +0700

    status

---
+++ status/script.js
@@ -0,0 +1,19 @@
+const games = {
+  "sdv": "100%",
+  "sve": "100%",
+  "pane": "100%",
+  "hand": "35%",
+  "hades": "17%"
+};
+
+let multiply = 4;
+
+Object.entries(games).forEach(([game, pourcent]) => {
+  const delay = 700;
+  
+  setTimeout(() => {
+    document.getElementById(`${game}-pourcent`).textContent = pourcent;
+  }, delay * multiply);
+  
+  multiply++;
+});
