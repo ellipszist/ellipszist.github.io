d41e3970df7a101f5ab238dae39d9c6d77b351e6
ELL <ellipszist@gmail.com>
2023-04-10 20:48:20 +0700

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, onBackgroundMessage } from "firebase/messaging";
-
-// Initialize Firebase app
-const firebaseConfig = {
-  // Firebase project configuration
-  apiKey: "AIzaSyB0mys5Hj0qt7lUgKFiDRYkDfbgOS7nAp8",
-  authDomain: "ell-store-630f8.firebaseapp.com",
-  projectId: "ell-store-630f8",
-  storageBucket: "ell-store-630f8.appspot.com",
-  messagingSenderId: "705373234131",
-  appId: "1:705373234131:web:2c5a52b052292452278526",
-  measurementId: "G-R629QDWGBQ"
-};
-
-const app = initializeApp(firebaseConfig);
-
-// Get Firebase Cloud Messaging instance
-const messaging = getMessaging(app);
-
-// Handle incoming messages when app is in background
-onBackgroundMessage((payload) => {
-  console.log('[Service Worker] Received background message:', payload);
-
-  // Customize notification title and body
-  const notificationTitle = 'Firebase Push Notification';
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  };
-
-  // Show the notification
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-// Cache the app shell
 const CACHE_NAME = 'my-app-cache';
 const urlsToCache = [
   '/',
@@ -43,58 +7,49 @@ const urlsToCache = [
   '/images/junispring.png',
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing service worker...');
 
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(function(cache) {
         console.log('[Service Worker] Caching app shell...');
         return cache.addAll(urlsToCache);
-      })
-      .catch((error) => {
-        console.error('[Service Worker] Failed to cache app shell:', error);
       })
   );
 });
 
 // Clean up old caches
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating service worker...');
 
   event.waitUntil(
     caches.keys()
-      .then((keyList) => {
-        return Promise.all(keyList.map((key) => {
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
           if (key !== CACHE_NAME) {
             console.log('[Service Worker] Removing old cache:', key);
             return caches.delete(key);
           }
         }));
       })
-      .catch((error) => {
-        console.error('[Service Worker] Failed to remove old caches:', error);
-      })
   );
 });
 
 // Serve cached app shell or fetch from network
-self.addEventListener('fetch', (event) => {
-  console.log('[Service Worker] Fetch event:', event.request.url);
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching:', event.request.url);
 
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then(function(response) {
         if (response) {
-          console.log('[Service Worker] Serving cached response:', event.request.url);
+          console.log('[Service Worker] Serving from cache:', event.request.url);
           return response;
         }
 
-        console.log('[Service Worker] Fetching new response:', event.request.url);
+        console.log('[Service Worker] Fetching from network:', event.request.url);
         return fetch(event.request);
-      })
-      .catch((error) => {
-        console.error('[Service Worker] Failed to fetch response:', error);
       })
   );
 });
