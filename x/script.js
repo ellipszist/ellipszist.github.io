846d9a86169c6647f2f50efe53b12d90e1c26278
ELL <ellipszist@gmail.com>
2023-04-13 01:52:17 +0700

    Update script.js

--- x/script.js
+++ x/script.js
@@ -67,3 +67,81 @@ document.getElementById('query').value = query;
 
 // Trigger the search function
 searchBox.dispatchEvent(new Event('input'));
+
+// Disable right-click on the page
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Add context menu to all images with the "shareable" class
+const shareableImages = document.querySelectorAll('.shareable');
+let previousMenu = null; // Reference to previous menu element
+let longPressTimer = null; // Timer for long press
+shareableImages.forEach(image => {
+  image.addEventListener('mousedown', event => {
+    if (event.button === 2) {
+      showContextMenu(event, image);
+    } else {
+      longPressTimer = setTimeout(() => {
+        showContextMenu(event, image);
+      }, 1000); // Long press duration is 1 second
+    }
+  });
+  image.addEventListener('mouseup', event => {
+    clearTimeout(longPressTimer);
+  });
+  image.addEventListener('touchstart', event => {
+    longPressTimer = setTimeout(() => {
+      showContextMenu(event, image);
+    }, 1000); // Long press duration is 1 second
+  });
+  image.addEventListener('touchend', event => {
+    clearTimeout(longPressTimer);
+  });
+});
+
+// Show context menu at the position of the mouse click or touch event
+function showContextMenu(event, image) {
+  event.preventDefault(); // Prevent default context menu
+  if (previousMenu) previousMenu.remove(); // Remove previous menu
+  const menu = document.createElement('div'); // Create a new menu
+  menu.classList.add('menu'); // Add CSS class to menu
+  menu.innerHTML = `
+    <a href="#" onclick="copyUrl(event, 'https://ellipszist.github.io/x/beta.html?query=${encodeURIComponent(image.alt)}');">คัดลอกลิงก์ม็อด</a>
+    <a href="#" onclick="shareByEmail(event, 'https://ellipszist.github.io/x/beta.html?query=${encodeURIComponent(image.alt)}', '${encodeURIComponent(image.alt)}');">แบ่งปันทางอีเมล</a>
+    <a href="#" onclick="shareOnFacebook(event, 'https://ellipszist.github.io/x/beta.html?query=${encodeURIComponent(image.alt)}', '${encodeURIComponent(image.alt)}');">แชร์บน Facebook</a>
+    <a href="#" onclick="shareOnTwitter(event, 'https://ellipszist.github.io/x/beta.html?query=${encodeURIComponent(image.alt)}', '${encodeURIComponent(image.alt)}');">แชร์บนทวิตเตอร์</a>
+  `; // Add share options to menu
+  menu.style.top = `${event.pageY}px`; // Position menu at cursor
+  menu.style.left = `${event.pageX}px`;
+  document.body.appendChild(menu); // Add menu to the DOM
+  document.addEventListener('click', () => {
+    menu.remove(); // Remove menu on click outside
+    previousMenu = null; // Clear previous menu reference
+  }, { once: true });
+  previousMenu = menu; // Set current menu as previous menu
+}
+
+// Copy URL to clipboard and display message
+function copyUrl(event, url) {
+  event.preventDefault(); // Prevent link click
+  navigator.clipboard.writeText(url); // Copy URL to clipboard
+  //alert('คัดลอก URL ไปยังคลิปบอร์ดแล้ว'); // Display message
+}
+
+function shareByEmail(event, url, alt) {
+  event.preventDefault();
+  const subject = encodeURIComponent(`Check out this page: ${decodeURIComponent(alt)}`);
+  const body = encodeURIComponent(`I thought you might find this page interesting: ${url}`);
+  window.location.href = `mailto:?subject=${subject}&body=${body}`;
+}
+
+function shareOnFacebook(event, url, alt) {
+  event.preventDefault();
+  const shareUrl = `https://www.facebook.com/sharer.php?u=${encodeURIComponent(url)}&quote=${decodeURIComponent(alt)}`;
+  window.open(shareUrl, '_blank');
+}
+
+function shareOnTwitter(event, url, alt) {
+  event.preventDefault();
+  const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${decodeURIComponent(alt)}`;
+  window.open(shareUrl, '_blank');
+}
