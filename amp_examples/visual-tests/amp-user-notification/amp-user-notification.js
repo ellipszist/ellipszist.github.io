fd2f2f02e6ae2533a01cca9cb23b73ff48d63aaa
ELL <ellipszist@gmail.com>
2024-06-09 22:19:33 +0700

    Delete unused files

--- amp_examples/visual-tests/amp-user-notification/amp-user-notification.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  'dismiss user notification': async (page, unusedName) => {
-    await page.tap('amp-user-notification>button');
-    await page.waitForTimeout(500);
-  },
-};
--- assets/js/scripts.js
+++
@@ -1,5 +0,0 @@
-/* Sticky Navbar - Vanilla JS */
-function stickyElement(a){a=document.querySelector(".header");a=getComputedStyle(a).height.split("px")[0];var b=document.querySelector(".navigation"),c=window.scrollY;c>a?b.classList.add("is-fixed"):c<a&&b.classList.remove("is-fixed")}window.addEventListener("scroll",stickyElement);
-
-/* Autoplay audio with mute and play */
-var pause=document.querySelector(".pause"),audio=document.querySelector(".audio");function togglePlay(){audio.paused?(audio.play(),pause.innerHTML="🔇"):(audio.pause(),pause.innerHTML="🏝️",pause.style.color="#848484")};
